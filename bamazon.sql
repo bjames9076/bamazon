e03d4f7476a4f4ad7d5622fc72a939d84c59b9c4
Brandon James <bjames9076@gmail.com>
2018-01-15 20:13:46 -0500

    added products

--- bamazon.sql
+++ bamazon.sql
@@ -10,3 +10,17 @@ CREATE TABLE IF NOT EXISTS `products` (
   `stock_quantity` int(100) DEFAULT NULL,
   PRIMARY KEY (`item_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+
+INSERT INTO `products` (`item_id`, `product_name`, `department_name`, `price`, `stock_quantity`) VALUES
+	(1, 'IPOD', 'Electronics', 599.99, 10),
+	(2, 'Keyboard', 'Electronics', 39.99, 15),
+	(3, 'Eggs', 'Food', 2.99, 50),
+	(4, 'Ear Buds', 'Electronics', 49.99, 12),
+	(5, 'TV', 'Electronics', 299.99, 6),
+	(6, 'Tennis Rackets', 'Sports', 17.99, 20),
+	(7, 'T-shirt', 'Clothes', 15.99, 25),
+	(8, 'Milk', 'Food', 2.49, 200),
+	(9, 'Dresser', 'Home', 199.99, 3),
+	(10, 'Basketball', 'Sports', 19.99, 10),
+	(11, 'Desk', 'Home', 99.99, 5),
+	
