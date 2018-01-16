ae957e4b7aed243355294f9e639479721ad0eb95
Brandon James <bjames9076@gmail.com>
2018-01-15 20:31:40 -0500

    added exit function

--- customer.js
+++ customer.js
@@ -2,3 +2,13 @@ var mysql = require('mysql');
 var inquirer = require('inquirer');
 var colors = require('colors');
 var Table = require('cli-table');
+
+
+//Contains the host, port, user, password, and database to be used for in the mySQL database
+var keys = require('./keys.js')
+
+var connection = mysql.createConnection(keys.connection);
+
+connection.connect(function(err) {
+    if (err) throw err;
+});
