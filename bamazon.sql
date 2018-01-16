3d000922c06fd99b811ed93df35a1fb48c5f37e8
Brandon James <bjames9076@gmail.com>
2018-01-15 20:03:49 -0500

    created table

--- bamazon.sql
+++ bamazon.sql
@@ -1,3 +1,12 @@
 CREATE DATABASE bamazon;
 
 USE bamazon;
+
+CREATE TABLE IF NOT EXISTS `products` (
+  `item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(100) NOT NULL,
+  `department_name` varchar(100) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `stock_quantity` int(100) DEFAULT NULL,
+  PRIMARY KEY (`item_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
